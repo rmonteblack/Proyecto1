1cc929af198970f1868f753c51a9eea265fe3ca6
Rene Montenegro <montenegrosoto9@gmail.com>
2024-08-28 08:30:25 -0600

    se empezo a hacer la logica del programa

---
+++ Proyecto1/Estacionamiento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1
+{
+    internal class Estacionamiento
+    {
+        private List<Vehiculos> vehiculos = new List<Vehiculos>();
+        private int espaciosDisponibles = 50;
+
+        public void RegistrarVehiculo()
+        {
+            if (espaciosDisponibles >0)
+            {
+
+            }
+        }
+    }
+}
--- Proyecto1/Program.cs
+++ Proyecto1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel.Design;
+using Proyecto1;
+
+static void Mains (string[]args)
+{
+    Estacionamiento estacionamiento = new Estacionamiento ();
+    Menu();
+}
+
+static void Menu ()
+{
+    while (true)
+    {
+        Console.WriteLine("Bievenido al parque landivar");
+        Console.WriteLine("Por favor selecccione una opción para continuar");
+    }
+}
